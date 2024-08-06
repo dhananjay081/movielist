eefbe47e107836e5d1d375598948b5975afa7af0
dhananjay <dhananjaykushwaha238@gamil.com>
2024-08-06 18:55:47 +0530

    updating the header

--- src/Components/Headers/header.jsx
+++ src/Components/Headers/header.jsx
@@ -9,10 +9,14 @@ function Header() {
     setIsOpen(!isOpen);
   };
 
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div className='header fixed top-0 left-0 w-full bg-transparent z-50 shadow-md'>
       <div className='flex items-center justify-between p-4 min-[480px]:gap-8 backdrop-blur-lg'>
-        <Link to="/">
+        <Link to="/" onClick={closeMenu}>
           <img className='h-12 w-12 min-[480px]:h-16 min-[480px]:w-16 rounded-lg' src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" alt="IMDB Logo" />
         </Link>
         <div className='min-[480px]:hidden'>
@@ -21,9 +25,9 @@ function Header() {
           </button>
         </div>
         <div className={`flex-col min-[480px]:flex-row gap-4 min-[480px]:gap-8 min-[480px]:flex ${isOpen ? 'flex' : 'hidden'}`}>
-          <Link to="/movies/popular" className='text-white hover:text-gray-300'>Popular</Link>
-          <Link to="/movies/top_rated" className='text-white hover:text-gray-300'>Top Rated</Link>
-          <Link to="/movies/upcoming" className='text-white hover:text-gray-300'>Upcoming</Link>
+          <Link to="/movies/popular" className='text-white hover:text-gray-300' onClick={closeMenu}>Popular</Link>
+          <Link to="/movies/top_rated" className='text-white hover:text-gray-300' onClick={closeMenu}>Top Rated</Link>
+          <Link to="/movies/upcoming" className='text-white hover:text-gray-300' onClick={closeMenu}>Upcoming</Link>
         </div>
       </div>
     </div>
