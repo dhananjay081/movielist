8f7a672898e0da051bb9ac3fc5466ca9c6c89b1b
dhananjay <dhananjaykushwaha238@gamil.com>
2024-08-06 20:52:39 +0530

    making resposive in movielist

--- src/Components/movieList/movielist.jsx
+++ src/Components/movieList/movielist.jsx
@@ -18,8 +18,8 @@ function Movielist() {
     };
 
     return (
-        <div className="movie_list p-4 sm:p-8 md:p-12">
-            <h2 className='list_title text-xl sm:text-2xl md:text-3xl m-4 sm:m-6 md:m-10'>{(type ? type : "POPULAR").toUpperCase()}</h2>
+        <div className="movie_list pt-20 p-4 sm:pt-30 sm:pr-8 sm:pl-8 sm:pb-8   md:pt-30 md:pr-8 md:pl-8 md:pb-8 ">
+           <h2 className="list_title text-2xl md:text-3xl font-extrabold text-gradient bg-clip-text bg-transparent m-6">{(type ? type : "POPULAR").toUpperCase()}</h2>
             <div className='list_cards grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 justify-center'>
                 {
                     movielist.map((movie, index) => (
